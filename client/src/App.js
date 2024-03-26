1aff1badefa9a7ef468114be0e772be150405eae
Josh Rivera <beps@Mactop.local>
2024-03-26 18:48:47 -0500

    Created Navigation component and initialized on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <h1>Cool App Stuff Here</h1>
+
     </div>
   );
 }
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default Navigation
