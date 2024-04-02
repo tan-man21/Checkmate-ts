1105574df81b8dcda334e64d0db9a84937b5ee74
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-04-02 19:27:10 -0400

    added CRUD routes to lists controller

--- server/controllers/lists_controller.js
+++ server/controllers/lists_controller.js
@@ -2,6 +2,7 @@
 const lists = require('express').Router()
 const db = require('../models')
 const { List } = db
+const { Op } = require('sequelize')
 
 //FIND ALL LISTS
 lists.get('/', async (req, res) => {
@@ -9,7 +10,7 @@ lists.get('/', async (req, res) => {
         const foundLists = await List.findAll()
         res.status(200).json(foundLists)
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error.message)
     }
 })
 
@@ -21,7 +22,43 @@ lists.get('/:id', async (req, res) => {
         })
         res.status(200).json(foundList)
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error.message)
+    }
+})
+
+//CREATE A LIST
+lists.post('/', async (req, res) => {
+    try {
+        const newList = await List.create(req.body)
+        res.status(200).json(newList)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+
+//UPDATE LIST
+lists.put('/:id', async (req, res) => {
+    try {
+        const {list_name} = req.body
+        const updatedList = await List.update(
+            { list_name },
+            { where: { list_id: req.params.id } }
+        )
+        res.status(200).json('Updated List!')
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+
+//DELETE LIST
+lists.delete('/:id', async (req, res) => {
+    try {
+        const deletedList = await List.destroy(
+            { where: { list_id: req.params.id } }
+        )
+        res.status(200).json('Deleted List!')
+    } catch (error) {
+        res.status(500).json(error.message)
     }
 })
 
