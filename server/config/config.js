90796814e07af9dfa5061994e20aa7debf6c3d7f
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-04-03 09:07:35 -0400

    updated config to be more dynamic, updated list controller do update lists, finished subtask controller

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: "postgres",
+    username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: "localhost",
