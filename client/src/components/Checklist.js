76de477f44b9c4fd8ed4b52a17435e808d29a836
CyanFaux <santafox26@gmail.com>
2024-04-04 19:16:09 -0400

    tinkering with subitems

--- client/src/components/Checklist.js
+++ client/src/components/Checklist.js
@@ -8,13 +8,12 @@ const Checklist = () => {
   const [editItemId, setEditItemId] = useState(null);
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([
-    { id: 1, text: "", checked: false, subItems: [] },
+    { id: "0", text: "", checked: false, subItems: [] },
   ]);
   const inputRef = useRef(null);
 
-// removes text input box when clicked outside
+  // removes text input box when clicked outside
   useEffect(() => {
-
     const handleClickOutside = (event) => {
       if (inputRef.current && !inputRef.current.contains(event.target)) {
         whenClickedAway();
@@ -27,22 +26,20 @@ const Checklist = () => {
     };
   }, []);
 
-
-//opens text input box when clicked on
+  //opens text input box when clicked on
   const whenClickedOn = (id, event) => {
-    if (event.target.tagName === 'INPUT') return;
+    if (event.target.tagName === "INPUT") return;
     if (editItemId === id) {
       return;
     }
     setEditItemId(id);
   };
 
-
   const whenClickedAway = () => {
     setEditItemId(null);
   };
 
-// sets text in input box to new text and accounts for sub items if necessary
+  // sets text in input box to new text and accounts for sub items if necessary
   const updateText = (id, newText, parentId = null) => {
     if (parentId === null) {
       setItems((prevItems) =>
@@ -66,7 +63,7 @@ const Checklist = () => {
     }
   };
 
-// switches the checkmark state of an item
+  // switches the checkmark state of an item
   const toggleChecked = (id) => {
     setItems((prevItems) =>
       prevItems.map((item) =>
@@ -74,7 +71,6 @@ const Checklist = () => {
       )
     );
   };
-
 
   const createNewListItem = () => {
     const newListItem = {
@@ -89,28 +85,49 @@ const Checklist = () => {
   };
 
 
+  const generateSubItemId = (parentId, subItemCount) => {
+    return `${parentId}${subItemCount}`;
+  };
+
   const createNewSubItem = (parentId) => {
-    const parentItemIndex = items.findIndex((item) => item.id === parentId);
-    if (parentItemIndex !== -1) {
-      const newSubItem = {
-        id: items[parentItemIndex].subItems.length + 1,
-        text: inputText,
-      };
-      setItems((prevItems) => {
+    setItems((prevItems) => {
+      // find index of parent item in array
+      const parentItemIndex = prevItems.findIndex(
+        (item) => item.id === parentId
+      );
+      if (parentItemIndex !== -1) {
+
+        // create child id if found
+        const itemCount = prevItems[parentItemIndex].subItems.length + 1
+        const subItemId = generateSubItemId(parentId, itemCount)
+
+        // characteristics of new sub item
+        const newSubItem = {
+          id: subItemId,
+          text: inputText,
+          checked: false,
+        }
+        // new array for sub items attached to parent item
         const updatedItems = [...prevItems];
         updatedItems[parentItemIndex].subItems.push(newSubItem);
+        setInputText("");
+        setEditItemId(newSubItem.id);
         return updatedItems;
-      });
-      setInputText("");
-      setEditItemId(newSubItem.id);
-    }
+      }
+      // return unchanged array if unfound
+      return prevItems;
+    });
   };
 
   return (
     <div>
       <ol className="checkList">
         {items.map((item) => (
-          <li className="mainListItem" key={item.id} onClick={(event) => whenClickedOn(item.id, event)}>
+          <li
+            className="mainListItem"
+            key={item.id}
+            onClick={(event) => whenClickedOn(item.id, event)}
+          >
             <input
               type="checkbox"
               className="checkBox"
@@ -126,8 +143,12 @@ const Checklist = () => {
                 ref={inputRef}
               />
             ) : (
-              <span className={item.checked ? "checkedItem" : ""}
-              style={{ textDecoration: item.checked ? "line-through" : "none" }}>
+              <span
+                className={item.checked ? "checkedItem" : ""}
+                style={{
+                  textDecoration: item.checked ? "line-through" : "none",
+                }}
+              >
                 {item.text || "Add Item"}
               </span>
             )}
@@ -136,7 +157,10 @@ const Checklist = () => {
                 <li key={subItem.id}>{subItem.text}</li>
               ))}
               <li className="addSubItem">
-                <button className="addSubItemButton" onClick={() => createNewSubItem(item.id)}>
+                <button
+                  className="addSubItemButton"
+                  onClick={() => createNewSubItem(item.id)}
+                >
                   Add Sub Item
                 </button>
               </li>
@@ -146,7 +170,9 @@ const Checklist = () => {
         ))}
 
         <li className="addItem">
-          <button className="addItemButton" onClick={createNewListItem}>+</button>
+          <button className="addItemButton" onClick={createNewListItem}>
+            +
+          </button>
         </li>
       </ol>
     </div>
